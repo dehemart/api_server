7f3f6f09f43cc095224705c27529690d8b920bb5
dehemart <martins.denis@gmail.com>
2026-06-27 11:59:18 -0300

    chore: addming constants to env

--- lib/api/routes/sign_in_route.dart
+++ lib/api/routes/sign_in_route.dart
@@ -4,9 +4,9 @@ import 'package:shelf/shelf.dart';
 
 class SignInRoute {
   Future<Response> handler(Request req) async {
-    final jwt = JWT({'userId': '12345', 'role': 'admin'}, issuer: 'http://localhost');
+    final jwt = JWT({'userId': '12345', 'role': 'admin'}, issuer: 'http:// $Env().dbHost)');
 
-    final token = jwt.sign(SecretKey(Env().jwtSecret), expiresIn: Duration(minutes: Env().jwtRefreshExpiresMinuts));
+    final token = jwt.sign(SecretKey(Env().jwtSecret), expiresIn: Duration(minutes: Env().jwtSessionExpires));
 
     return Response.ok('{"token": "$token"}', headers: {'content-type': 'application/json'});
   }
--- lib/config/env.dart
+++ lib/config/env.dart
@@ -1,11 +1,21 @@
 import 'package:dotenv/dotenv.dart';
 
+const String _defaultServerPort = '8080';
+const String _defaultJwtSecret = 'no_jwt_secret_loaded';
+const int _defaultJwtSessionExpires = 15;
+const int _defaultJwtRefreshExpires = 120;
+const String _defaultDatabaseHost = 'localhost';
+
 final dotEnv = DotEnv(includePlatformEnvironment: true);
 
 class Env {
-  String get serverPort => dotEnv['SERVER_PORT'] ?? '8080';
-  String get jwtSecret => dotEnv['JWT_SECRET'] ?? 'no_jwt_secret_loaded';
-  int get jwtSessionExpiresMinuts => int.tryParse(dotEnv['JWT_SESSION_EXPIRY_MINUTS'] ?? '15') ?? 15;
-  int get jwtRefreshExpiresMinuts => int.tryParse(dotEnv['JWT_SESSION_EXPIRY_MINUTS'] ?? '120') ?? 120;
-  String get dbHost => dotEnv['DB_HOST'] ?? 'localhost';
+  String get dbHost => dotEnv['DB_HOST'] ?? _defaultDatabaseHost;
+  String get serverPort => dotEnv['SERVER_PORT'] ?? _defaultServerPort;
+  String get jwtSecret => dotEnv['JWT_SECRET'] ?? _defaultJwtSecret;
+  int get jwtSessionExpires =>
+      int.tryParse(dotEnv['JWT_SESSION_EXPIRY_MINUTS'] ?? _defaultJwtSessionExpires.toString()) ??
+      _defaultJwtSessionExpires;
+  int get jwtRefreshExpires =>
+      int.tryParse(dotEnv['JWT_SESSION_EXPIRY_MINUTS'] ?? _defaultJwtRefreshExpires.toString()) ??
+      _defaultJwtRefreshExpires;
 }
